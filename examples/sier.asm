e1b3a83bae5a7593ce4d798d552efb9013bc5eb4
Rhys Weatherley <rhys.weatherley@gmail.com>
2026-03-30 16:14:08 +1000

    Optimise the Sierpiński triangle

--- examples/sier.asm
+++ examples/sier.asm
@@ -1,84 +1,67 @@
 ;
 ; Sierpiński triangle for LGP-21.
 ;
-    .org    1300
     .entry  start
-start:
-    ld      size
-    st      y
+.1300:  start:          ld      size
+.1301:                  st      y
 ;
-outer_loop:
-    ld      zero
-    st      tmp
+.1302:  outer_loop:     ld      zero
+.1303:                  st      tmp
 ;
-print_spaces:
-    ld      space
-    pr6
-    ld      tmp
-    add     one
-    st      tmp
-    sub     y
-    jn      print_spaces
+.1304:  print_spaces:   ld      space
+.1305:                  pr6
+.1306:                  ld      tmp
+.1307:                  add     one
+.1308:                  st      tmp
+.1309:                  sub     y
+.1310:                  jn      print_spaces
 ;
-    ld      zero
-    st      x
-    ld      y
-    st      tmp
+.1311:                  ld      zero
+.1312:                  st      x
+.1313:                  ld      y
+.1314:                  st      tmp
 ;
-print_points:
-    ld      x
-    and     y
-    sub     one
-    jn      print_point
-    ld      space
-    pr6
-    pr6
-    jmp     next_point
+.1315:  print_points:   ld      x
+.1316:                  and     y
+.1317:                  sub     one
+.1318:                  jn      print_point
+.1319:                  ld      space
+.1320:                  pr6
+.1321:                  pr6
+.1322:                  jmp     next_point
 ;
-print_point:
-    ld      show_point
-    pr6
-    shl6
-    pr6
+.1323:  print_point:    ld      show_point
+.1324:                  pr6
+.1325:                  shl6
+.1326:                  pr6
 ;
-next_point:
-    ld      x
-    add     one
-    st      x
-    ld      tmp
-    add     one
-    st      tmp
-    sub     sizep1
-    jn      print_points
+.1327:  next_point:     ld      x
+.1328:                  add     one
+.1329:                  st      x
+.1330:                  ld      tmp
+.1331:                  add     one
+.1332:                  st      tmp
+.1333:                  sub     sizep1
+.1334:                  jn      print_points
 ;
-    ld      newline
-    pr6
-    ld      y
-    sub     one
-    st      y
-    jn      done
-    jmp     outer_loop
-done:
-    hlt
-
-size:
-    .dw     #124     ; 31 * 4
-sizep1:
-    .dw     #128     ; (31 + 1) * 4
-zero:
-    .dw     0
-one:
-    .dw     0001
-show_point:
-    .dw     "+ "
-space:
-    .dw     " "
-newline:
-    .dw     "\n"
+.1335:                  ld      newline
+.1336:                  pr6
+.1337:                  ld      y
+.1338:                  sub     one
+.1339:                  st      y
+.1340:                  jn      done
+.1341:                  jmp     outer_loop
+;
+.1342:  done:           hlt
+;
+.1343:  size:           .dw     #124     ; 31 * 4
+.1344:  sizep1:         .dw     #128     ; (31 + 1) * 4
+.1345:  show_point:     .dw     "+ "
+.1346:  one:            .dw     0001
+.1347:  zero:           .dw     0000
+.1348:  space:          .dw     " "
+.1349:  newline:        .dw     "\n"
     .noemit
-x:
-    .dw     0
-y:
-    .dw     0
-tmp:
-    .dw     0
+.1350:  tmp:            .dw     0
+.1351:  x:              .dw     0
+.1352:  y:              .dw     0
