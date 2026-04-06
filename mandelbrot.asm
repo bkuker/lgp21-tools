4950f2f9101650f4e2e1fc597b3cc855bd2f10cf
bkuker <bkuker@billkuker.com>
2026-04-06 08:24:05 -0400

    faster

--- mandelbrot.asm
+++ mandelbrot.asm
@@ -1,19 +1,7 @@
     .org 1000
     .entry start
 
-iLow:   .dw -1.1 >> 4
-iHigh:  .dw 1.1 >> 4
-iStep:  .dw 0.1 >> 4
-iPos:   .dw 0
 
-rLow:   .dw -2.1 >> 4
-rHigh:  .dw 0.52 >> 4
-rStep:  .dw 0.040 >> 4
-rPos:   .dw 0;
-
-eol:    .dw "\n"
-dot:    .dw "."
-star:   .dw " "
 
                             ;Start a new fractal                    
 start:      ld  iLow        ;Reset iPos to iLow
@@ -42,17 +30,6 @@ nextChar:   ld  rPos        ;rPos += rStep
             jmp newLine
 
 
-max:    .dw #28
-dec:    .dw #2
-itr:    .dw 0
-x:      .dw 0
-y:      .dw 0
-xTemp:  .dw 0
-xx:     .dw 0
-yy:     .dw 0
-twoF:   .dw 2.0 >> 4
-fourF:  .dw 4.0 >> 4
-
 compute:    ld  max         ;itr = max
             stc itr
             st x            ; x = 0
@@ -63,16 +40,23 @@ iterate:    ld  y           ; yy = y * y
             mulh y
             shl4
             st yy
+            nop
             ld x            ; x*x
+            nop
+            nop
             mulh x
             shl4
             st xx
+            jmp 1039
+.1039:
             sub yy          ; ... - y*y
             add rPos        ; + x0
             st  xTemp
 
                             ; y := 2*x*y + y0
             ld twoF
+            jmp 1048
+.1048:
             mulh x
             shl4
             mulh y
@@ -80,35 +64,55 @@ iterate:    ld  y           ; yy = y * y
             add iPos
             st y
 
-                            ; x := xtemp
-            ld xTemp
+            ld xTemp        ; x := xtemp
             st x
 
             ld itr          ;Decrement Iterator
             sub dec
             st itr
 
-            jnt in         ;Iterator went negative, we are IN
+            jnt in          ;Iterator went negative, we are IN
 
-            ld xx
-            add yy
+            ld yy           ;Are we still inside the 2-unit circle?
+            add xx
             sub fourF
-            jnt iterate         ;Left the circle, we are out
-
+            jnt iterate     ;Left the circle, we are out
             jmp out
 
 
-in:         ld star
+
+out:        ld  itr
+            shl6
+            shl6
+            shl6
+            shl6
+            shl4
+            pr4
+            jmp nextChar
+
+in:         ld space
             pr6
             jmp nextChar
 
-out:        ld  itr
-            shl4
-            shl4
-            shl4
-            shl4
-            shl6
-            shl6
-            ;shl4
-            pr4
-            jmp nextChar
+                    .org 2000
+.2045:	iPos:	.dw 0
+.2062:	rPos:	.dw 0;
+.2043:	dec:	.dw #2
+.2051:	itr:	.dw 0
+.2013:	x:	    .dw 0
+.2025:	y:	    .dw 0
+.2006:	xTemp:	.dw 0
+.2047:	xx:	    .dw 0
+.2018:	yy:	    .dw 0
+.2021:	twoF:	.dw 2.0	>>	4
+.2055:	fourF:	.dw 4.0	>>	4
+.2035:	rStep:	.dw 0.040	>>	4
+.2024:	iHigh:	.dw 1.1	>>	4
+.2030:	rHigh:	.dw 0.52	>>	4
+.2026:	eol:	.dw "\n"
+.2052:	space:	.dw " "
+.2029:	iStep:	.dw 0.1	>>	4
+
+.2136:	iLow:	.dw -1.1	>>	4
+.2031:	rLow:	.dw -2.1	>>	4
+.2033:	max:	.dw #28
