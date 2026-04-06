cbe3681e64cb84c4e5ae8fc823c914b44f842d9d
bkuker <bkuker@billkuker.com>
2026-04-06 08:44:00 -0400

    formatting

--- mandelbrot.asm
+++ mandelbrot.asm
@@ -1,10 +1,46 @@
+;Vertical range & size
+.2136:	iLow:	.dw -1.1 >> 4
+.1224:	iHigh:	.dw 1.1 >> 4
+.1229:	iStep:	.dw 0.1 >> 4
+
+;Horizontal range & size
+.1231:	rLow:	.dw -2.1 >> 4
+.1230:	rHigh:	.dw 0.52 >> 4
+.1235:	rStep:	.dw 0.040 >> 4
+
+;Maximum number of iterations
+.1233:	max:	.dw #28
+
+;Current position
+.1245:	iPos:	.dw 0
+.1262:	rPos:	.dw 0;
+
+;Iteration variables
+.1251:	itr:	.dw 0
+.1213:	x:	    .dw 0
+.1206:	xTemp:	.dw 0
+.1247:	xx:	    .dw 0
+.1225:	y:	    .dw 0
+.1218:	yy:	    .dw 0
+
+;Constants for computation
+.1243:	dec:	.dw #2
+.1221:	twoF:	.dw 2.0 >> 4
+.1255:	fourF:	.dw 4.0 >> 4
+
+;Constants for output
+.1226:	eol:	.dw "\n"
+.1252:	space:	.dw " "
+
     .org 1000
     .entry start
-                            ;Start a new fractal                    
-start:      ld  iLow        ;Reset iPos to iLow
+
+start:                      ;Start a new fractal  
+            ld  iLow        ;Reset iPos to iLow
             st  iPos
-                            ;Start a new line
-newLine:    ld  rLow        ;Reset rPos to rLow
+
+newLine:                    ;Start a new line
+            ld  rLow        ;Reset rPos to rLow
             st  rPos
             
             ld  eol         ;Print a new line
@@ -18,22 +54,25 @@ newLine:    ld  rLow        ;Reset rPos to rLow
             jnt nextChar    ;else goto nextChar:
             hlt             ; TODO jmp start
 
-nextChar:   ld  rPos        ;rPos += rStep
+nextChar:                   ;Work out the next character
+            ld  rPos        ;rPos += rStep
             add rStep
             st  rPos
 
-            sub rHigh       ;if rPos > rHigh goto newLine
-            jnt compute
-            jmp newLine
+            sub rHigh       ;if rPos < rHigh
+            jnt compute     ;   goto compute
+            jmp newLine     ;else goto newLine
 
 
-compute:    ld  max         ;itr = max
+compute:
+            ld  max         ;itr = max
             stc itr
             st x            ; x = 0
             st y            ; y = 0
 
-                            ; xtemp := x^2 - y^2 + x0
-iterate:    ld  y           ; yy = y * y
+iterate:
+            ; xtemp := x^2 - y^2 + x0    
+            ld  y           ; yy = y * y
             mulh y
             shl4
             st yy
@@ -67,7 +106,6 @@ iterate:    ld  y           ; yy = y * y
             ld itr          ;Decrement Iterator
             sub dec
             st itr
-
             jnt in          ;Iterator went negative, we are IN
 
             ld yy           ;Are we still inside the 2-unit circle?
@@ -76,7 +114,7 @@ iterate:    ld  y           ; yy = y * y
             jnt iterate     ;Left the circle, we are out
             jmp out
 
-out:        ld  itr
+out:        ld itr
             shl6
             shl6
             shl6
@@ -89,23 +127,3 @@ in:         ld space
             pr6
             jmp nextChar
 
-.2045:	iPos:	.dw 0
-.2062:	rPos:	.dw 0;
-.2043:	dec:	.dw #2
-.2051:	itr:	.dw 0
-.2013:	x:	    .dw 0
-.2025:	y:	    .dw 0
-.2006:	xTemp:	.dw 0
-.2047:	xx:	    .dw 0
-.2018:	yy:	    .dw 0
-.2021:	twoF:	.dw 2.0 >> 4
-.2055:	fourF:	.dw 4.0 >> 4
-.2035:	rStep:	.dw 0.040 >> 4
-.2024:	iHigh:	.dw 1.1 >> 4
-.2030:	rHigh:	.dw 0.52 >> 4
-.2026:	eol:	.dw "\n"
-.2052:	space:	.dw " "
-.2029:	iStep:	.dw 0.1 >> 4
-.2136:	iLow:	.dw -1.1 >> 4
-.2031:	rLow:	.dw -2.1 >> 4
-.2033:	max:	.dw #28
